cf22a352dd2a76d9fdb50cad016e0a74885633b3
Al Muhdil Karim <al.muhdil.0@gmail.com>
2024-08-12 05:39:07 +0700

    fix js

--- assets/page/home/home.js
+++ assets/page/home/home.js
@@ -3,15 +3,18 @@ let headnav_init = () => {
 
     var juston;
     let profil = document.querySelector('#nav-lektorer');
+    let topnav = document.querySelector('.hamburger');
+    let htbody = document.getElementsByTagName('body')[0];
 
     profil.addEventListener('click', ()=> {
         console.log('lektorer click');
         if (!juston) {
-            document.getElementsByTagName('body')[0].dataset.menu = 'normal';
+            htbody.dataset.menu = 'normal';
+            topnav.classList.add('active');
             juston = true;
         }
-        document.getElementsByTagName('body')[0].classList.toggle('menu-open');
-    });
+        htbody.classList.toggle('menu-open');
+    }, false );
 }
 
 headnav_init();
