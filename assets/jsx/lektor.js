e87187cb6e4bd8d2d7df370b0358f8d99a3f8dc8
Al Muhdil Karim <al.muhdil.0@gmail.com>
2024-08-16 15:29:44 +0700

    fix mobile

--- assets/jsx/lektor.js
+++ assets/jsx/lektor.js
@@ -4,6 +4,17 @@ import { init_darks } from "../mod/darkmod/dark.js"
 import { script_gen, vpage_mode, init_navis } from "../mod/lektorer/main.js"
 
 
+function fix_height() {
+    const documentHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+    }
+    window.addEventListener(‘resize’, documentHeight)
+    documentHeight()
+       
+}
+
+
 window.onload = () => {
 
     const path = window.location.pathname;
@@ -14,6 +25,8 @@ window.onload = () => {
         script_gen('css', 'assets/page/home/home.css' );
         script_gen('jsx', 'assets/page/home/home.js' );
     }
+
+    fix_height();
 
     // horizontal page 
     vpage_mode();
